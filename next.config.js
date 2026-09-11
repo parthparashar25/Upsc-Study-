4f5496407d83aedc54b0794848e9e3e201bf3c21
parthparashar25 <318063643+parthparashar25@users.noreply.github.com>
2026-09-11 13:43:27 +0530

    Embed Supabase env variables into nextConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   typescript: {
     // Material Tailwind 2.1.x has type definition clashes with @types/react 18.2+
     ignoreBuildErrors: true,
